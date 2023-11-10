71f439cd9e2af39c8433a58103680146605502d5
JuandDoe <103640999+JuandDoe@users.noreply.github.com>
2023-11-11 00:26:26 +0100

    Update Main.java
    **Improved customisation of scraping for the user**
    
    // BEFORE // 
    A simple scrape example
    
     // NOW // 
    The user can now chose dynamicly wich card he want to scrap

--- pikascrape/src/main/java/ape/fr/Main.java
+++ pikascrape/src/main/java/ape/fr/Main.java
@@ -1,19 +1,38 @@
 package ape.fr;
 
 import com.microsoft.playwright.*;
+import java.util.Scanner;
+
+
 public class Main {
+
     public static void main(String[] args) {
 
         try (Playwright playwright = Playwright.create()) {
             Browser browser = playwright.firefox().launch();
+            Scanner input = new Scanner(System.in);
+            System.out.println("Go to https://www.cardmarket.com/en/Pokemon/Products/Singles and chose a card chose a card\n " +
+                    "imput everything after \"Singles\" in the url of the card you want to scrape\n " +
+                    "Example : for 'https://www.cardmarket.com/en/Pokemon/Products/Singles/151/Venusaur-ex-V1-MEW003' => '/151/Venusaur-ex-V1-MEW003'\n");
+
+            // trim() erase the " " character caused by typing "enter" after .nextLine
+            String cardCustomLink = input.nextLine().trim();
+
+            final String baseLink = "https://www.cardmarket.com/en/Pokemon/Products/Singles;";
+
+            // Erase ";" caused by Strings concatenation
+            String fullCustomLink = (baseLink + cardCustomLink).replace(";", "");
+
             Page page = browser.newPage();
-            page.navigate("https://www.cardmarket.com/en/Pokemon/Products/Singles/151/Venusaur-ex-V1-MEW003");
+            page.navigate(fullCustomLink);
 
             // define variable to store the given CSS selector of the page and his content
-            String priceCard = page.locator("#articleRow1543373023 > div.col-offer.col-auto > div.price-container.d-none.d-md-flex.justify-content-end > div > div > span").innerText();
+            String priceCard = page.locator("#articleRow1543511636 > div.col-offer.col-auto > div.price-container.d-none.d-md-flex.justify-content-end > div > div > span").innerText();
 
             // display the price found into the scraped selector
             System.out.println(priceCard);
         }
     }
+
+
 }
