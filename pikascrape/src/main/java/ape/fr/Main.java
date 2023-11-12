8913d1c3db984bdf34980dc159a079c3299a6e0d
JuandDoe <103640999+JuandDoe@users.noreply.github.com>
2023-11-12 01:28:28 +0100

    Update Main.java
    **DYNAMIC ID CARD IMPLEMENTATION**
    
    //BEFORE//
    
    Since each cards offers are linked to a specific ID, having the ID hardcoded leaded to scraping issues each time the lowest offer for a given card changed.
    
    //NOW//
    
    The script is looking for the first "articleRow" ID of the page (the cheapest product). Then it scrape the price and display it by concatenate this ID and the proper CSS Selector.

--- pikascrape/src/main/java/ape/fr/Main.java
+++ pikascrape/src/main/java/ape/fr/Main.java
@@ -16,9 +16,11 @@ public class Main {
                     "Example : for 'https://www.cardmarket.com/en/Pokemon/Products/Singles/151/Venusaur-ex-V1-MEW003' => '/151/Venusaur-ex-V1-MEW003'\n");
 
             // trim() erase the " " character caused by typing "enter" after .nextLine
-            String cardCustomLink = input.nextLine().trim();
 
             final String baseLink = "https://www.cardmarket.com/en/Pokemon/Products/Singles;";
+
+            String cardCustomLink = input.nextLine().trim();
+
 
             // Erase ";" caused by Strings concatenation
             String fullCustomLink = (baseLink + cardCustomLink).replace(";", "");
@@ -26,12 +28,45 @@ public class Main {
             Page page = browser.newPage();
             page.navigate(fullCustomLink);
 
-            // define variable to store the given CSS selector of the page and his content
-            String priceCard = page.locator("#articleRow1543511636 > div.col-offer.col-auto > div.price-container.d-none.d-md-flex.justify-content-end > div > div > span").innerText();
+
+            // Find all elements with "id" attributes
+            ElementHandle[] elements = page.querySelectorAll("[id]").toArray(new ElementHandle[0]);
+
+            // Iterate through the elements and find the first one that matches the criteria
+            ElementHandle targetElement = null;
+
+            for (ElementHandle element : elements) {
+                String idAttributeValue = element.getAttribute("id");
+
+                // Check if the id contains "articleRow" followed by a random number
+                if (idAttributeValue != null && idAttributeValue.matches("articleRow\\d+")) {
+                    targetElement = element;
+                    break; // Stop searching when a matching element is found
+                }
+            }
+
+            if (targetElement != null) {
+                targetElement.click();
+                System.out.println("Clicked on the element with 'id' containing 'articleRow' and a random number: " + targetElement.getAttribute("id"));
+            } else {
+                System.out.println("No elements with 'id' containing 'articleRow' and a random number found.");
+            }
+
+           String cardId =  targetElement.getAttribute("id");
+
+            String lowestPriceSelector = " > div.col-offer.col-auto > div.price-container.d-none.d-md-flex.justify-content-end > div > div > span";
+
+            String fullId = ("#" + cardId + lowestPriceSelector);
+            fullId = fullId.replace(";", "");
+
 
             // display the price found into the scraped selector
+
+            String priceCard = page.locator(fullId).innerText();
+
             System.out.println(priceCard);
         }
+
     }
 
 
