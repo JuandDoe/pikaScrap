3c17233428b0e20baa117d8d18ee63e48d535a76
ant <ant.gschwind@gmail.com>
2024-09-10 14:40:58 +0200

    accountManager basic logic

---
+++ pikascrape/src/main/java/ape/fr/accountManager.java
@@ -0,0 +1,41 @@
+package ape.fr;
+
+import java.util.Scanner;
+
+public class accountManager {
+
+    static int stateOfaccessToPikaScrape;
+
+
+    public static void accessToPikaScrape() {
+
+            System.out.println("Chose the option (1/2/3)");
+            System.out.println("1)Launch as a guest (You can't use bookmarks)");
+            System.out.println("2)Login");
+            System.out.println("3)Register");
+            Scanner input = new Scanner(System.in);
+            stateOfaccessToPikaScrape = input.nextInt();
+
+        while (true){
+
+            if (stateOfaccessToPikaScrape == 1){
+                System.out.println("Launching as guest");
+                Main.scrapeACard();
+                input.nextLine();
+
+                if (stateOfaccessToPikaScrape == 2){
+                    System.out.println("Log into your account");
+                    Main.scrapeACard();
+                    input.nextLine();
+
+                    if (stateOfaccessToPikaScrape == 3){
+                        System.out.println("Create an account");
+                        Main.scrapeACard();
+                        input.nextLine();
+
+                    }
+                }
+            }
+        }
+    }
+}
